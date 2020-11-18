0c3b93de11700689f8d952936914bb41cdc70959
PowerPixel <medhy.fournier3@gmail.com>
2020-11-18 23:11:08 +0100

    Updated project, and added first attributes.

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TabletAreaRandomizer
-{
-    public class Class1
-    {
-    }
-}
---
+++ TabletAreaRandomizer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenTabletDriver.Plugin.Logging;
+using OpenTabletDriver.Plugin.Platform.Pointer;
+using OpenTabletDriver.Plugin.Output;
+using OpenTabletDriver.Plugin.Attributes;
+
+namespace TabletAreaRandomizer
+{
+    [PluginName("Tablet Area Randomizer")]
+    public class TabletAreaRandomizer : AbsoluteOutputMode
+    {
+
+        public override IVirtualTablet VirtualTablet {get;}
+        [Property("Minimum Width"), Unit("mm")]
+        public float minWidth { set; get; }
+
+        [Property("Minimum Height"), Unit("mm")]
+        public float minHeight { set; get; }
+
+        [Property("Maximum Width"), Unit("mm")]
+        public float maxWidth { set; get; }
+
+        [Property("Maximim Height"), Unit("mm")]
+        public float maxHeight { set; get; }
+
+        [Property("Randomization delay"), Unit("s")]
+        public int randDelay {set; get;}
+    }
+}
